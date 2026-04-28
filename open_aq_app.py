de9a0ff8e1d6603671c0913f550e6d1aa792705d
Elisa Mursu <emursu@edu.lapinamk.fi>
2026-04-28 15:20:41 +0300

    Tehty yhden päivän mittauksien haku

--- open_aq_app.py
+++ open_aq_app.py
@@ -1,26 +1,25 @@
 from flask import Flask, jsonify, request
 
+import open_aq_database_mysql
+
 # Haetaan open_aq_database
 import open_aq_database
 
 app = Flask(__name__)
 
-# Tähän jouduin hieman kyselemään apua tekoälyltä ja yritin netistä kans kattoa hieman mallia.
-# Jumituin tähän alun toteutukseen
-
 # Yhden päivän mittaukset
-@app.route('/measurements/<int:loc_id>', methods=['GET'])
-def get_daily_measurements(loc_id):
+@app.route('/measurements/<int:location_id>', methods=['GET'])
+def get_daily_measurements(location_id):
     # Päivän haku
     date = request.args.get('date')
-    result = open_aq_database.get_location_daily_measurements(loc_id, date)
+    result = open_aq_database_mysql.get_location_daily_measurements(location_id, date)
     return jsonify(result)
 
 # Valitun mittauspaikan kaikkien mittausten lukumäärä
 @app.route('/measurements/<int:loc_id>/count', methods=['GET'])
 def get_total_measurement(loc_id):
     # Funktio joka tekee count kyselyn
-    result = open_aq_database.get_total_place_measurement(loc_id)
+    result = open_aq_database_mysql.get_total_place_measurement(loc_id)
     return jsonify(result)
 
 # Valitun mittauspaikan ja sensorin päivittäinen mittauskeskiarvo
@@ -29,7 +28,7 @@ def get_daily_average(loc_id):
     # Sensorin ID ja päivä
     sensor_id = request.args.get('sensor_id')
     date = request.args.get('date')
-    result = open_aq_database.get_daily_avg_for_sensor(loc_id, sensor_id, date)
+    result = open_aq_database_mysql.get_daily_avg_for_sensor(loc_id, sensor_id, date)
     return jsonify(result)
 
 if __name__ == '__main__':
--- open_aq_database_mysql.py
+++ open_aq_database_mysql.py
@@ -19,11 +19,18 @@ def get_location_daily_measurements(loc_id, date_str):
             # Kysely
             # m = measurement
             # s = sensor
-            _query = (" SELECT * "
+            # l = location
+            # Haetaan yhden paikan mittaukset
+            _query = (" SELECT m.id, m.value, m.datetime, l.name as location_name"
                       " FROM measurements m"
+                      " JOIN sensors s ON m.sensor_id = s.id"
+                      " JOIN locations l ON s.location_id = l.id"
+                      # l.name/l.id = kummallakin voi hakea
+                      " WHERE (l.name = %s OR l.id = %s)" 
+                      " AND DATE(m.datetime) = %s"
                       )
 
-            cur.execute(_query )
+            cur.execute(_query, (loc_id, loc_id, date_str))
 
             # Palauttaa kaikki
             return cur.fetchall()
